2fc48023b5bf2cbb8d4ffd799e6983bb91043fb2
Michał Łaszczewski <michal@emikse.com>
2019-12-17 09:49:51 +0100

    users slugs

--- register.js
+++ register.js
@@ -147,6 +147,18 @@ definition.action({
     if(emailRow) throw evs.error('alreadyAdded')
     let {user, email, passwordHash, userData} = registerKeyRow
     userData.email = email
+    userData.slug = await service.triggerService('slugs', {
+      type: "CreateSlug",
+      group: "user",
+      to: user
+    })
+    await service.triggerService('slugs', {
+      type: "TakeSlug",
+      group: "user",
+      path: user,
+      to: user,
+      redirect: userData.slug
+    })
     emit("emailPassword", [{
       type: "keyUsed",
       key
